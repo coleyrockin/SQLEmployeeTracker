251f3bd24c96400b51f20ecf5eee2a7da483d606
coleyrockin <coleyrockin@aol.com>
2022-08-18 13:43:42 -0500

    WOO

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -23,16 +23,16 @@ VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Jane", "Doe", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Norah", "Rios", 2, 1);
+VALUES ("Norah", "Jones", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kate", "Mellor", 3, null);
+VALUES ("Kate", "Late", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Shiv", "Betts", 4, 3);
+VALUES ("Afrirwa", "Burgen", 4, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Leen", "Hurst", 5, null);
+VALUES ("Zeek", "McGee", 5, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Gary", "Leer", 2, null);
+VALUES ("John", "Steward", 2, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Cruise", 4, 7);
+VALUES ("Boyd", "Roberts", 4, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Daisy", "Hooper", 1, 2);
+VALUES ("Dane", "Rambo", 1, 2);
